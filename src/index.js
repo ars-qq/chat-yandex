8e4c1642291ee32f45adafe5292b4271084ff8ff
ars-qq <ars.qqztsq@gmail.com>
2022-11-04 19:17:46 +0300

    env test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import sum from "./modules/sum/sum.js"
 import range from "./utils/mydash/range.js"
 
-const arr = range(5, 25, 3)
+const arr = range(-6)
 
 const $node = document.getElementById(`root`);
 $node.textContent = arr.join(` : `);
